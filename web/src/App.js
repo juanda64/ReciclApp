ff05d58f40cc668d0b4724f8f97775d5890bf1d7
juanda64 <juandavpenilla@gmail.com>
2024-09-29 23:12:19 -0500

    instalación de i18next

--- web/src/App.js
+++ web/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useTranslation } from 'react-i18next';
+import { Suspense } from 'react';
 
 function App() {
   return (
-    <Home />
+    <Suspense fallback="Cargando traducciones">
+      <Home />
+    </Suspense>
   );
 }
 
--- web/src/index.js
+++ web/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./config/i18next.config";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
