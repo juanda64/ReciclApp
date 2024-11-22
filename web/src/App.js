0f3c00d9135c8d52d3c80b3bcd6ea11abc70c3ad
juanda64 <juandavpenilla@gmail.com>
2024-11-21 19:08:07 -0500

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Admin from './components/Admin';
 import Material from './components/Material';
+import GestionUser from './components/GestionUser';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/materiales" element={<Material/>} />
+          <Route path="/admin/usuarios" element={<GestionUser/>} />
         </Routes>
       </Router>
     </Suspense>
